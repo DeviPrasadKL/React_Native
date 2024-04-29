e7d6598dcfa7e03b5f7c87cba2617eb44e61a9ca
DeviPrasadKL <deviprasadoffcial@gmail.com>
2024-04-29 10:40:08 +0530

    Navigatin

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/Home';
+import About from './Components/About';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home/>
-      <Text style={{color:'white'}}>Hello </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} options={{
+          title: 'My home',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} />
+      <Stack.Screen name="About" component={About}  options={{
+          headerRight: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Alert"
+              color="#f4511e"
+            />
+          ),
+        }}/>
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
--- Components/Home.js
+++ Components/Home.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function Home() {
+export default function Home({navigation }) {
     const [data, setData] = useState("Hello");
     return (
         <View>
             <Text style={{ color: 'white' }}>From home </Text>
             <TextInput style={styles.inputTag} value={data} onChange={(e) => { setData(e.target.value) }} />
+
+            <Button title="Go to about"  onPress={() => navigation.navigate('About')} />
         </View>
     );
 }
