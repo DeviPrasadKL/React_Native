568e1172114d5de7e8c947bc02672f0684444198
DeviPrasadKL <deviprasadoffcial@gmail.com>
2024-04-30 10:09:28 +0530

    chore: kitten ui add

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout } from '@ui-kitten/components';
 import Home from './Components/Home';
 import About from './Components/About';
 
@@ -9,29 +11,31 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} options={{
-          title: 'My home',
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }} />
-      <Stack.Screen name="About" component={About}  options={{
-          headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              title="Alert"
-              color="#f4511e"
-            />
-          ),
-        }}/>
-    </Stack.Navigator>
-  </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{
+            title: 'My home',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }} />
+          <Stack.Screen name="About" component={About} options={{
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Alert"
+                color="#f4511e"
+              />
+            ),
+          }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
 
@@ -39,7 +43,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    color:'white',
+    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- Components/About.js
+++ Components/About.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Layout, Text } from '@ui-kitten/components';
 
 export default function About() {
+  const [counter, setCounter] = React.useState(0);
+
   return (
     <View>
-        <Text>About</Text>
+      <Layout
+        style={styles.container}
+        level='1'
+      >
+
+        <Button onPress={() => setCounter(counter + 1)}>
+          BUTTON
+        </Button>
+
+        <Text style={styles.text}>
+          {`Pressed ${counter} times`}
+        </Text>
+
+      </Layout>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    marginHorizontal: 8,
+  },
+});
